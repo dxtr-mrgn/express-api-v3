2cce2d220db701d5a8474757cf862e8351f755e6
Dexter Morgan <ck12.test.aa@gmail.com>
2025-02-02 07:59:35 -0500

    Update Vercel version

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ export const app = express();
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response): void => {
-    res.send({'Version': 'BLL 3.0'});
+    res.send({'Version': 'BLL 4.0'});
 });
 
 app.delete(SETTINGS.API.ALL_DATA, async (req: Request, res: Response): Promise<void> => {
